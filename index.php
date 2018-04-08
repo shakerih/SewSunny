e6cde2d862d5cc0a287ad07df89ef60380ba88c9
Unknown <yla312@sfu.ca>
2018-04-08 01:38:28 -0700

    index style

--- index.php
+++ index.php
@@ -15,38 +15,44 @@ require_once('initialize.php');
 
 <?php include('header.php'); ?>
 
-<div id="content" class="content_container">
+<div id="content">
   <?php
   if(is_logged_in()){
-  echo "<h2>Pinned Projects</h2>";
-$query = "SELECT projects.projectID, projects.projectTitle, projects.description, projects.tag, projects.imgURL FROM projects INNER JOIN (SELECT projectID FROM favourite_project WHERE userID=".$_SESSION['userID'].") AS fave ON projects.projectID = fave.projectID";
+    echo "<div class='img_cover'></div>";
+    echo "<div class='project_container'>";
+    echo "<h2>Pinned Projects</h2>";
+    $query = "SELECT projects.projectID, projects.projectTitle, projects.description, projects.tag, projects.imgURL FROM projects INNER JOIN (SELECT projectID FROM favourite_project WHERE userID=".$_SESSION['userID'].") AS fave ON projects.projectID = fave.projectID";
 
     $result = mysqli_query($connection, $query);
-    echo "<div class='project_container'>";
         while($row = mysqli_fetch_row($result)){ // add rows to the table
             echo "<div class='project_item'>";
             echo "<div class='project'>";
-            echo "<a href='modeldetails.php?projectCode=". $row[0]."'>" . $row[1] . "</a><br>";
+            // echo "<div class='img_overlay'></div>";
             echo "<img src='". $row[4] . "'><br>";
+            echo "<a href='modeldetails.php?projectCode=". $row[0]."'>" . $row[1] . "</a><br>";
             echo "</div>";
             echo "</div>";
         }
     echo "</div>";
   }
   else{
+    echo "<div class='img_cover'></div>";
+    echo "<div class='content_container'>";
     echo "<h2>Highest Rated Projects</h2>";
     $query = "SELECT projects.projectID, projects.projectTitle, projects.description, projects.tag, projects.imgURL FROM projects INNER JOIN (SELECT projectID FROM ratings ORDER BY rating DESC LIMIT 5) AS top ON projects.projectID = top.projectID";
         $result = mysqli_query($connection, $query);
         echo "<div class='project_container'>";
-            while($row = mysqli_fetch_row($result)){ // add rows to the table
-                echo "<div class='project_item'>";
-                echo "<div class='project'>";
-                echo "<a href='modeldetails.php?projectCode=". $row[0]."'>" . $row[1] . "</a><br>";
-                echo "<img src='". $row[4] . "'><br>";
-                echo "</div>";
-                echo "</div>";
-            }
-        echo "</div>";
+        while($row = mysqli_fetch_row($result)){ // add rows to the table
+            echo "<div class='project_item'>";
+            echo "<div class='project'>";
+            // echo "<div class='img_overlay'></div>";
+            echo "<img src='". $row[4] . "'><br>";
+            echo "<a href='modeldetails.php?projectCode=". $row[0]."'>" . $row[1] . "</a><br>";
+            echo "</div>";
+            echo "</div>";
+        }
+    echo "</div>";
+    echo "</div>";
   }
   ?>
 </div>
