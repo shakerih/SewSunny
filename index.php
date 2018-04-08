537de72cc24403c31ab50c36eee56eb0cccdbc34
Hanieh Shakeri <hshakeri@sfu.ca>
2018-04-07 19:13:34 -0700

    displays top 5 project on entry page for visitors

--- index.php
+++ index.php
@@ -22,17 +22,25 @@ require_once('initialize.php');
 $query = "SELECT projects.projectID, projects.projectTitle, projects.description, projects.tag, projects.imgURL FROM projects INNER JOIN (SELECT projectID FROM favourite_project WHERE userID=".$_SESSION['userID'].") AS fave ON projects.projectID = fave.projectID";
 
     $result = mysqli_query($connection, $query);
-        while($row = mysqli_fetch_row($result)){ // add rows to the table 
-         
+        while($row = mysqli_fetch_row($result)){ // add rows to the table
+
             echo "<a href='modeldetails.php?projectCode=". $row[0]."'>" . $row[1] . "</a><br>";
             echo "<img src='". $row[4] . "'><br>";
-          
+
         }
 
   }
   else{
+    echo "<h1>Highest Rated Projects</h1><br>";
+    $query = "SELECT projects.projectID, projects.projectTitle, projects.description, projects.tag, projects.imgURL FROM projects INNER JOIN (SELECT projectID FROM ratings ORDER BY rating DESC LIMIT 5) AS top ON projects.projectID = top.projectID";
+        $result = mysqli_query($connection, $query);
+            while($row = mysqli_fetch_row($result)){ // add rows to the table
+
+                echo "<a href='modeldetails.php?projectCode=". $row[0]."'>" . $row[1] . "</a><br>";
+                echo "<img src='". $row[4] . "'><br>";
+
+            }
 
   }
   ?>
 </div>
-
