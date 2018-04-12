1ceeba711616d55ede0992f2369b24a2df014ea5
Unknown <yla312@sfu.ca>
2018-04-11 22:48:13 -0700

    checking rating to display highest rated projects

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 
 <div id="content">
     <?php
+    // if member is logged in, show pin board
     if(is_logged_in()){
         echo "<div class='header_space'></div>";
         echo "<div class='content_container'>";
@@ -37,11 +38,14 @@
         echo "</div>";
         echo "</div>";
     }
+    // if member is not logged in, show highest rated projects
     else{
         echo "<div class='img_cover'></div>";
         echo "<div class='content_container'>";
         echo "<h2>Highest Rated Projects</h2>";
-        $query = "SELECT projects.projectID, projects.projectTitle, projects.description, projects.tag, projects.imgURL, category.categoryName, members.username FROM projects INNER JOIN category ON projects.categoryID = category.categoryID INNER JOIN members ON projects.userID=members.userID INNER JOIN (SELECT projectID FROM ratings ORDER BY rating DESC LIMIT 5) AS top ON projects.projectID = top.projectID";
+        $query = "SELECT projects.projectID, projects.projectTitle, projects.description, projects.tag, projects.imgURL, category.categoryName, members.username FROM projects INNER JOIN category ON projects.categoryID = category.categoryID INNER JOIN members ON projects.userID=members.userID INNER JOIN (SELECT projectID, sum(rating)/COUNT(*) FROM ratings GROUP BY projectID ORDER BY rating DESC LIMIT 5) AS top ON projects.projectID = top.projectID";
+        // this groups the rated project with same id and calculate the rating
+        // SELECT projectID, sum(rating)/COUNT(*) FROM ratings GROUP BY projectID 
 
 
         echo "<div class='project_container'>";
